c0cca5114167a4db04dd9bad051bc1380ebf9653
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2025-06-23 21:00:04 +0200

    docker arm

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 # docker build --progress=plain --no-cache -f docs/dev-pasienky/Dockerfile .
 
 cd docs/dev-pasienky
+
+# vycitenie disku - ak je malo miesta na disku, moze build spadnut
+#sudo apt-get clean
+docker image prune -f
+docker container prune -f
+
 docker compose up --build --force-recreate -d
